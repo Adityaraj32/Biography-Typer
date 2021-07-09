9932435f63fdeb041b94ec8746e0d03ac2471962
Aditya raj <87028110+Adityaraj32@users.noreply.github.com>
2021-07-09 07:55:17 +0530

    Add files via upload

---
+++ Biography_info.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+father = input("Enter your father name: ")
+mother = input("Enter your mother name: ")
+born = input("Enter your birth date,month and year: ")
+address = input("Enter your Address: ")
+state = input("Enter your state: ")
+country = input("Enter your country: ")
+
+print(f"Hello my name is {name}, and my father name is {father},my mohter name is {mother},\ni was born in {born} i live in {address},{state} {country}")
